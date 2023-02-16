2b687c080018c06084c3061d44351a8e566632f0
Lukasz Wisniewski <wsa3@vp.pl>
2023-02-16 07:01:19 +0000

    Added welcome funcion to JS

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let currentCoursElement = document.querySelector(".js-currentCourse");
-let resultElement = document.querySelector(".js-result");
+{
+    function welcome() {
+        console.log("welcome in my currency exchange");
+    }
+      welcome();
 
-formElement.addEventListener("submit", (event) =>{
-    event.preventDefault();
+
+    const formElement = document.querySelector(".js-form");
+    const amountElement = document.querySelector(".js-amount");
+    const currentCoursElement = document.querySelector(".js-currentCourse");
+    const resultElement = document.querySelector(".js-result");
     
-    let amount = amountElement.value;
-    let currentCourse = currentCoursElement.value;
-    let result = amount*currentCourse;
-    resultElement.innerText = result.toFixed(2);
-} );
+    formElement.addEventListener("submit", (event) =>{
+        event.preventDefault();
+        
+        const amount = amountElement.value;
+        const currentCourse = currentCoursElement.value;
+        const result = amount*currentCourse;
+        resultElement.innerText = result.toFixed(2);
+    } );
+ 
+   
+
+}

0d43e12ef4e2671e62f05caf2008ab2a1de489a8
Lukasz Wisniewski <wsa3@vp.pl>
2023-02-19 13:02:03 +0000

    another function added to JS

--- script.js
+++ script.js
@@ -3,27 +3,18 @@
         console.log("welcome in my currency exchange");
     }
       welcome();
-
-      const updateResultText = (result) =>{
-
-        const resultElement = document.querySelector(".js-result");
-        resultElement.innerText = result.toFixed(2);
-
-      }
       
     const onFormSubmit = (event) =>{
     event.preventDefault();
     const amountElement = document.querySelector(".js-amount");
     const currentCoursElement = document.querySelector(".js-currentCourse");
-  
+    const resultElement = document.querySelector(".js-result");
 
     const amount = amountElement.value;
     const currentCourse = currentCoursElement.value;
     const result = amount*currentCourse;
-
-    onFormSubmit(result);
-    
-}; 
+    resultElement.innerText = result.toFixed(2);
+} 
   
     const init = () =>{
    

cd59804512dde2fce5ac3931ea3d23560914483a
Lukasz Wisniewski <wsa3@vp.pl>
2023-02-19 12:56:54 +0000

    anoyher functions added to JS

--- script.js
+++ script.js
@@ -4,20 +4,36 @@
     }
       welcome();
 
+      const updateResultText = (result) =>{
 
-    const formElement = document.querySelector(".js-form");
+        const resultElement = document.querySelector(".js-result");
+        resultElement.innerText = result.toFixed(2);
+
+      }
+      
+    const onFormSubmit = (event) =>{
+    event.preventDefault();
     const amountElement = document.querySelector(".js-amount");
     const currentCoursElement = document.querySelector(".js-currentCourse");
-    const resultElement = document.querySelector(".js-result");
+  
+
+    const amount = amountElement.value;
+    const currentCourse = currentCoursElement.value;
+    const result = amount*currentCourse;
+
+    onFormSubmit(result);
     
-    formElement.addEventListener("submit", (event) =>{
-        event.preventDefault();
-        
-        const amount = amountElement.value;
-        const currentCourse = currentCoursElement.value;
-        const result = amount*currentCourse;
-        resultElement.innerText = result.toFixed(2);
-    } );
+}; 
+  
+    const init = () =>{
+   
+    const formElement = document.querySelector(".js-form");
+
+    formElement.addEventListener("submit", onFormSubmit);
+
+   };
+
+   init();
  
    
 
